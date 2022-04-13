8a8feb95e1e379d9560160283e4d82ea88bc92ed
Keriou21 <williamandre09@gmail.com>
2022-04-13 16:55:22 +0200

    fin db ?

--- profil/models.py
+++ profil/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
 
@@ -10,29 +11,27 @@ class Promotion(models.Model):
 class Notes(models.Model):
     notes_value = models.FloatField(max_length=4)
     def __str__(self):
-        return self.notes_value
+        return str(self.notes_value)
 
 class Average(models.Model):
     average_value = models.FloatField(max_length=4)
     def __str__(self):
-        return self.average_value
+        return str(self.average_value)
 
 class Classes(models.Model):
     classes_name = models.CharField(max_length=200)
     def __str__(self):
         return self.classes_name
 
-class Images(models.Model):
-    pass
-
 class Student(models.Model):
     student_firstname = models.CharField(max_length=200)
     student_lastname = models.CharField(max_length=200)
-    #student_birth = models.DateField(default=None)
+    #student_birth = models.DateTimeField('birth_date')
     student_promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
     student_notes = models.ManyToManyField(Notes)
     student_classes = models.ManyToManyField(Classes)
     student_average = models.ForeignKey(Average, on_delete=models.CASCADE)
-    student_image = models.ForeignKey(Images, on_delete=models.CASCADE)
+    student_image = models.ImageField()
+
     def __str__(self) :
-        return "{} {}, {} ans, promotion {}, notes : {}, cours : {}, moyenne {}".format(self.student_firstname, self.student_lastname, self.student_age, Promotion.__str__(self), Notes.__str__(self), Classes.__str__(self), Average.__str__(self))
+        return "{} {}, promotion {}, notes : {}, cours : {}, moyenne {}".format(self.student_firstname, self.student_lastname, self.student_promotion, self.student_notes, self.student_classes, self.student_average)
